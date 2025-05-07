a6806b76bc95f968349cfac330713d8ccbc52ced
sofie.adriana@gmail.com <sofie.adriana@gmail.com>
2025-05-07 13:53:42 +0000

    grouped by date

---
+++ models/Intermediate/int_campaigns_day.sql
@@ -0,0 +1,8 @@
+ select
+ date_date
+ ,sum(ads_cost) as ads_cost
+ ,sum(impression) as ads_impression
+ ,sum(click) as ads_clicks
+ from {{ref('int_campaigns')}}
+ group by date_date
+ order by date_date desc
