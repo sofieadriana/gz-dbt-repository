14f7a1fad1773a887bd32d4c8f0939a5c399e8a2
sofie.adriana@gmail.com <sofie.adriana@gmail.com>
2025-05-07 12:52:06 +0000

    Added column names for ads_cost

--- models/staging/raw/stg_raw__bing.sql
+++ models/staging/raw/stg_raw__bing.sql
@@ -13,7 +13,7 @@ renamed as (
         paid_source,
         campaign_key,
         campgn_name AS campaign_name,
-        CAST(ads_cost AS FLOAT64),
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
         impression,
         click
 
